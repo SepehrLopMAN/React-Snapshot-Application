31b21e08afefaf9f946dbe3a63624959871e768a
Sepehr Katebi <sepehrkatebi61@gmail.com>
2023-02-21 16:48:52 +0400

    Initial commit 0.1 Modifiable shape added Input ranges with 'OnChange' functionality (for border-radius values) added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import styled, { css } from "styled-components";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const Container = styled.div`
+  position: relative;
+  border: 2px dashed grey;
+  width: 480px;
+  height: 480px;
+  margin: 2rem auto;
+`;
+
+const ModifiableShape = styled.div.attrs(
+  ({ $borderRadius: { top, bottom, left, right } }) => {
+    return {
+      style: {
+        borderRadius: `${top}% ${100 - top}% ${
+          100 - bottom
+        }% ${bottom}% / ${left}% ${right}% ${100 - right}% ${100 - left}%`,
+      },
+    };
+  }
+)`
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(45deg, darkblue, deeppink);
+  box-shadow: -100px 100px 0px 0px #ffffff12;
+  // ???????????????????????????????
+`;
+
+const RangeInput = styled.input.attrs({
+  type: "range",
+})`
+  & {
+    width: calc(100% + 1rem + 4px);
+    position: absolute;
+    ${({ $right, $left }) =>
+      ($right || $left) &&
+      css`
+        transform-origin: left;
+        transform: rotate(90deg);
+      `}
+  }
+  &:foucos {
+    outline: none;
+  }
+
+  &:-webkit-thumb-slider {
+    appearance: none;
+    border: 2px solid red;
+  }
+`;
+
+const App = () => {
+  const [BRState, dispatch] = useReducer(
+    (state, modifierObject) => {
+      console.log(modifierObject, state, state[`${modifierObject.side}`]);
+      if (state[`${modifierObject.side}`] === undefined) {
+        throw Error("Unknown Action: " + modifierObject);
+      }
+      return {
+        ...state,
+        [`${modifierObject.side}`]: modifierObject.newValue,
+      };
+    },
+    { top: 10, left: 20, bottom: 30, right: 40 }
   );
-}
+
+  function onChangeHandler(target) {
+    return ({ target: { value } }) => {
+      dispatch({ side: target, newValue: value });
+    };
+  }
+  return (
+    <Container>
+      <ModifiableShape $borderRadius={BRState} />
+      <RangeInput
+        value={BRState.top}
+        onChange={onChangeHandler("top")}
+        style={{ top: "calc(-0.5rem - 4px)", left: "calc(-0.5rem - 4px)" }}
+      />
+      <RangeInput
+        $left={true}
+        value={BRState.left}
+        onChange={onChangeHandler("left")}
+        style={{ top: "calc(-0.5rem - 0.5rem - 4px)", left: "calc(-4px)" }}
+      />
+      <RangeInput
+        $right={true}
+        value={BRState.right}
+        onChange={onChangeHandler("right")}
+        style={{ top: "calc(-0.5rem - 0.5rem - 4px)", left: "calc(100%)" }}
+      />
+      <RangeInput
+        value={BRState.bottom}
+        onChange={onChangeHandler("bottom")}
+        style={{
+          bottom: "calc(-0.5rem - 4px)",
+          left: "calc(-0.5rem - 4px)",
+        }}
+      />
+    </Container>
+  );
+};
 
 export default App;
