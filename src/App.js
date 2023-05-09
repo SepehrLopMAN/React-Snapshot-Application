03564c0de69eefb576baff2e63f0940e514ac5c4
Sepehr Katebi <sepehrkatebi61@gmail.com>
2023-05-09 15:37:49 +0400

    Patch commit 2.4.2 Footer added UI bugs fixed

--- src/App.js
+++ src/App.js
@@ -117,6 +117,13 @@ const App = () => {
         settersObject={{ setCustomizable, setHeightValue, setWidthValue }}
         elemWrapperRef={wrapperRef}
       />
+      <footer className="footer-container">
+        <div className="footer-content">
+          <span className="footer-content--copyright-symbol">&copy;</span>
+          Created by
+          <h6 className="footer-content__author-heading"> Sepehr Katebi</h6>
+        </div>
+      </footer>
     </>
   );
 };
--- src/components/styled/Global.js
+++ src/components/styled/Global.js
@@ -1,19 +1,23 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-    body {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-        sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        background-color: #000022;
-        color: white;
-        font-size: 1.25rem;
-        margin: auto auto 4rem;
-        min-width: 350px;
-        overflow-x: hidden;
+  
+html {
+    min-width: fit-content;
+  }
+
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #000022;
+    color: white;
+    font-size: 1.25rem;
+    min-width: 350px;
+    overflow-x: hidden;
   }
 
   label {
